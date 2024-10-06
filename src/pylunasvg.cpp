e0a2631774e1e754effa405ddd884692bc6ff78d
eren <erntkn4@gmail.com>
2024-10-06 22:30:55 +0300

    few updates

--- src/pylunasvg.cpp
+++ src/pylunasvg.cpp
@@ -1,6 +1,7 @@
 ﻿#include <string>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
 #include "lunasvg.h"
 
 namespace py = pybind11;
@@ -9,21 +10,59 @@ using namespace lunasvg;
 PYBIND11_MODULE(pylunasvg, m)
 {
 	m.doc() = "pylunasvg is python bindings for lunasvg library";
+    py::class_<Matrix> matrix(m, "Matrix");
 	py::class_<Box>(m, "Box")
 		.def(py::init<>())
-		.def(py::init<double, double, double, double>(), 
-			py::arg("x"), py::arg("y"), py::arg("w"), py::arg("h"));
+		.def(py::init<double, double, double, double>(),
+			py::arg("x"), py::arg("y"), py::arg("w"), py::arg("h"))
+        .def("transform", &Box::transform, py::arg("matrix"))
+        .def("transformed", &Box::transformed, py::arg("matrix"))
+        .def_readwrite("x", &Box::x)
+        .def_readwrite("y", &Box::y)
+        .def_readwrite("w", &Box::w)
+        .def_readwrite("h", &Box::h);
 
-	py::class_<Matrix>(m, "Matrix")
-	.def(py::init<>())
-	.def(py::init<double, double, double, double, double, double>(), 
-		py::arg("a"), py::arg("b"), py::arg("c"), py::arg("d"), py::arg("e"), py::arg("f"));
+    py::class_<Matrix>(matrix, "Matrix")
+        .def(py::init<>())
+        .def(py::init<double, double, double, double, double, double>(),
+             py::arg("a"), py::arg("b"), py::arg("c"), py::arg("d"), py::arg("e"), py::arg("f"))
+        //.def(py::init<const Transform&>(), py::arg("transform"))
+        .def("rotate", py::overload_cast<double>(&Matrix::rotate),
+            py::arg("angle"))
+        .def("rotate", py::overload_cast<double, double, double>(&Matrix::rotate),
+             py::arg("angle"), py::arg("cx"), py::arg("cy"))
+        .def("scale", &Matrix::scale,
+             py::arg("sx"), py::arg("sy"))
+        .def("shear", &Matrix::shear,
+             py::arg("shx"), py::arg("shy"))
+        .def("translate", &Matrix::translate,
+             py::arg("tx"), py::arg("ty"))
+        .def("transform", &Matrix::transform,
+             py::arg("a"), py::arg("b"), py::arg("c"), py::arg("d"), py::arg("e"), py::arg("f"))
+        .def("identity", &Matrix::identity)
+        .def("invert", &Matrix::invert)
+        .def(py::self *= Matrix())
+        .def("premultiply", &Matrix::premultiply)
+        .def("postmultiply", &Matrix::postmultiply)
+        .def("inverted", &Matrix::inverted)
+        .def(py::self * Matrix())
+        .def_static("rotated", py::overload_cast<double>(&Matrix::rotated))
+        .def_static("rotated", py::overload_cast<double, double, double>(&Matrix::rotated))
+        .def_static("scaled", &Matrix::scaled, py::arg("sx"), py::arg("sy"))
+        .def_static("sheared", &Matrix::scaled, py::arg("shx"), py::arg("shy"))
+        .def_static("translated", &Matrix::scaled, py::arg("tx"), py::arg("ty"))
+        .def_readwrite("a", &Matrix::a)
+        .def_readwrite("b", &Matrix::b)
+        .def_readwrite("c", &Matrix::c)
+        .def_readwrite("d", &Matrix::d)
+        .def_readwrite("e", &Matrix::e)
+        .def_readwrite("f", &Matrix::f);
 
-	py::class_<Bitmap>(m, "Bitmap", py::buffer_protocol())
+    py::class_<Bitmap>(m, "Bitmap", py::buffer_protocol())
 		.def(py::init<>())
-		.def(py::init<std::uint8_t*, std::uint32_t, std::uint32_t, std::uint32_t>(), 
+		.def(py::init<std::uint8_t*, std::uint32_t, std::uint32_t, std::uint32_t>(),
 			py::arg("data"), py::arg("width"), py::arg("height"), py::arg("stride"))
-		.def(py::init<std::uint32_t, std::uint32_t>(), 
+		.def(py::init<std::uint32_t, std::uint32_t>(),
 			py::arg("width"), py::arg("height"))
 		.def("reset", py::overload_cast<std::uint8_t*, std::uint32_t, std::uint32_t, std::uint32_t>(&Bitmap::reset),
 			 py::arg("data"), py::arg("width"), py::arg("height"), py::arg("stride"))
@@ -59,31 +98,12 @@ PYBIND11_MODULE(pylunasvg, m)
 		.def_static("loadFromData", py::overload_cast<const char*>(&Document::loadFromData),
 			py::arg("data"), 
 			"Creates a document from a null terminated string data")
-		.def("rotate", py::overload_cast<double>(&Document::rotate), 
-			py::arg("angle"), 
-			"Rotates the document matrix clockwise around the current origin")
-		.def("rotate", py::overload_cast<double, double, double>(&Document::rotate), 
-			py::arg("angle"), py::arg("cx"), py::arg("cy"), 
-			"Rotates the document matrix clockwise around the given point")
-		.def("scale", &Document::scale, 
-			py::arg("sx"), py::arg("sy"), 
-			"Scales the document matrix by sx horizontally and sy vertically")
-		.def("shear", &Document::shear, 
-			py::arg("shx"), py::arg("shy"), 
-			"Shears the document matrix by shx horizontally and shy vertically")
-		.def("translate", &Document::translate, 
-			py::arg("tx"), py::arg("ty"), 
-			"Translates the document matrix by tx horizontally and ty vertically")
-		.def("transform", &Document::transform, 
-			py::arg("a"), py::arg("b"), py::arg("c"), py::arg("d"), py::arg("e"), py::arg("f"),
-			 "Multiplies the document matrix by Matrix(a, b, c, d, e, f)")
-		.def("identity", &Document::identity, "Resets the document matrix to identity")
 		.def("matrix", &Document::matrix, "Returns the current transformation matrix of the document")
 		.def("box", &Document::box, "Returns the smallest rectangle in which the document fits")
 		.def("width", &Document::width, "Returns width of the document")
 		.def("height", &Document::height, "Returns the height of the document")
 		.def("render", &Document::render, 
-			py::arg("bitmap"), py::arg("matrix") = Matrix{}, py::arg("backgroundColor") = 0x00000000, 
+			py::arg("bitmap"), py::arg("matrix") = Matrix{},
 			"Renders the document to a bitmap")
 		.def("renderToBitmap", &Document::renderToBitmap, 
 			py::arg("width") = 0, py::arg("height") = 0, py::arg("backgroundColor") = 0x00000000, 
